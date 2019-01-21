9d6d7786c62cf3b205728ffcad74d8f267b5842d
s33dco <code@s33d.co>
2019-01-21 10:19:16 +0000

    update with async await and error handling

--- currency-convert.js
+++ currency-convert.js
@@ -1,31 +1,74 @@
 const axios           = require('axios');
 
-const getExchangeRate =  (from, to) => {
-  return axios.get(`https://free.currencyconverterapi.com/api/v6/convert?q=${from}_${to}`) // return promise
-  .then((response) => {
-    return response.data.results[`${from}_${to}`].val;                                     // return value
-  });
-}
+const getExchangeRate =  async (from, to) => {
+  try {
+    const response = await axios.get(`https://free.currencyconverterapi.com/api/v6/convert?q=${from}_${to}`) // return promise
+    const rate = response.data.results[`${from}_${to}`].val;
 
-const getCountries = (currencyCode) => {
-  return axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`)
-  .then((response) => {
+    if (rate) {                   // this catches the error if no rate
+      return rate                 // convertCurrencyAlt('notcurrencycode', "EUR", 100) would give undefined error withouththis block
+    } else {
+      throw new Error();
+    }
+  } catch (e) {
+    throw new Error(`Unable to get exchange rate for ${from} and ${to}`)
+  }
+};
+
+const getCountries    =  async (currencyCode) => {
+  try {
+    const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`)
     return response.data.map((country) => country.name);
-  });
-}
+  } catch (e) {
+    throw new Error(`Unable to get countries that use ${currencyCode}`)
+  }
+};
 
 // USD CAD 23
-
+//
 // 23 usd is worth 28 cad. you spend these in the following countries....
 
-getExchangeRate('GBP', 'EUR').then((rate) => {
-  console.log(rate);
-}).catch((e) => {
-  console.log(e.message);
-});
+// getExchangeRate('GBP', 'EUR').then((rate) => {
+//   console.log(rate);
+// }).catch((e) => {
+//   console.log(e.message);
+// });
+//
+// getCountries('EUR').then((countries) => {
+//   console.log(countries);
+// }).catch((e) => {
+//   console.log(e.message);
+// });
 
-getCountries('EUR').then((countries) => {
-  console.log(countries);
+const convertCurrency = (from, to, amount) => {
+  let countries;
+  return getCountries(to).then((tempCountries) => {
+    countries = tempCountries;
+    return getExchangeRate(from, to);
+  }).then((rate) => {
+    const exchangeAmount = amount * rate;
+    return `${amount} ${from} is worth ${exchangeAmount} ${to}.\n${to} can be used in the following countries: ${countries.join(', ')}`;
+  });
+}
+
+const convertCurrencyAlt = async (from, to, amount) => {
+  const countries = await getCountries(to);
+  const rate = await getExchangeRate(from, to);
+  const exchangeAmount = amount * rate;
+  return `${amount} ${from} is worth ${exchangeAmount} ${to}.\n${to} can be used in the following countries: ${countries.join(', ')}`;
+};
+
+//
+// convertCurrency('GBP', "EUR", 100).then((summary) => {
+//   console.log(summary);
+// }).catch((e) => {
+//   console.log(e.message);
+// });
+//
+
+
+convertCurrencyAlt('USD', "EUR", 100).then((summary) => {
+  console.log(summary);
 }).catch((e) => {
   console.log(e.message);
 });
